d9908d97a300fd5157f15a5a2824db9fcadbd18a
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-05-26 15:00:16 +0100

    complete html outline set up reset + variable scss

---
+++ data/countries.js
@@ -0,0 +1,24 @@
+export const countriesArr = [
+    {
+        country : "france",
+        flag: "🇫🇷",
+        continent: "europe"
+    },
+     {
+        country : "germany",
+        flag: "🇩🇪",
+        continent: "europe"
+    },
+    {
+        country : "spain",
+        flag: "🇪🇸",
+        continent: "europe"
+    },
+    {
+        country : "italy",
+        flag: "🇮🇹",
+        continent: "europe"
+    }
+];
+
+export default countriesArr;
--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log(1)
+const countriesArr = [
+    {
+        country : "france",
+        flag: "🇫🇷",
+        continent: "europe"
+    },
+     {
+        country : "germany",
+        flag: "🇩🇪",
+        continent: "europe"
+    },
+    {
+        country : "spain",
+        flag: "🇪🇸",
+        continent: "europe"
+    },
+    {
+        country : "italy",
+        flag: "🇮🇹",
+        continent: "europe"
+    }
+];
+
+
+console.log(countriesArr[0]);
